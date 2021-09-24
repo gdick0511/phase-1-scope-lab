d75b5d75e356a95acdbd8bcf1e87bc0a4de17ac0
garrettdick <garrettd0511@gmail.com>
2021-09-23 21:07:42 -0500

    commit tree 8db176521653c72f8db8cd1209ce69a23c84ca5c

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'Karen'
+
+function upperCaseCustomerName(){
+    return customerName ='bob'.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Greg'
+}
